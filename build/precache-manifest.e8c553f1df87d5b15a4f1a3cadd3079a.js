2ec5f2995db09db00018f3779733e7bc5c30393c
alex-bul <50155885+alex-bul@users.noreply.github.com>
2020-03-20 01:39:12 +0200

    Add files via upload

---
+++ build/precache-manifest.e8c553f1df87d5b15a4f1a3cadd3079a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa934a3af307a84c5810e07047116fec",
+    "url": "/alex-bul/alex-bul.github.io/index.html"
+  },
+  {
+    "revision": "141be2ac9e2260a26e72",
+    "url": "/alex-bul/alex-bul.github.io/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "141be2ac9e2260a26e72",
+    "url": "/alex-bul/alex-bul.github.io/static/js/2.a415604c.chunk.js"
+  },
+  {
+    "revision": "abf0b813b0144a2f8f7c56edfbc053fa",
+    "url": "/alex-bul/alex-bul.github.io/static/js/2.a415604c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95891654f6f771cc96dc",
+    "url": "/alex-bul/alex-bul.github.io/static/js/main.11381dd8.chunk.js"
+  },
+  {
+    "revision": "0542e9687af9cbfbbea7",
+    "url": "/alex-bul/alex-bul.github.io/static/js/runtime-main.663627b5.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/alex-bul/alex-bul.github.io/precache-manifest.e8c553f1df87d5b15a4f1a3cadd3079a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alex-bul/alex-bul.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
